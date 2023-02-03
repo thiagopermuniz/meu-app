4ede77004199763a205ce4bcf8e9d5a5fd3f6bc8
thiagop <thiagopermuniz@gmail.com>
2023-02-02 23:06:04 -0300

    add hello contrller

---
+++ meu-app-java/src/main/java/com/br/meu_app_java/MeuAppJavaApplication.java
@@ -0,0 +1,13 @@
+package com.br.meu_app_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuAppJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeuAppJavaApplication.class, args);
+    }
+
+}
---
+++ meu-app-java/src/test/java/com/br/meu_app_java/MeuAppJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.meu_app_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuAppJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
